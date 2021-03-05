c70965894ab74f9d099262234b296e2c6a61c717
bbogeul <illumeweb@gmail.com>
2021-03-05 15:10:23 +0900

    added kb food category table

--- src/modules/data/index.ts
+++ src/modules/data/index.ts
@@ -1,2 +1,5 @@
 export * from './location-analysis/location-anaylsis.module';
 export * from './menu-analysis/menu-analysis.module';
+
+// entities
+export * from './entities';
--- src/modules/data/menu-analysis/menu-analysis.service.ts
+++ src/modules/data/menu-analysis/menu-analysis.service.ts
@@ -1,7 +1,19 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { PaginatedRequest, PaginatedResponse } from 'src/common';
 import { BaseService } from 'src/core';
+import { Repository } from 'typeorm';
+import { KbFoodCategoryGroup } from '../entities';
+import { AdminKbFoodCategoryGroupListDto } from './dto';
 
 export class MenuAnalysisService extends BaseService {
-  constructor() {
+  constructor(
+    @InjectRepository(KbFoodCategoryGroup, 'wq')
+    private readonly kbFoodCategoryGroupRepo: Repository<KbFoodCategoryGroup>,
+  ) {
     super();
   }
+
+  // async findAllForAdmin(adminKbFoodCategoryGroupListDto: AdminKbFoodCategoryGroupListDto, pagination: PaginatedRequest): Promise<PaginatedResponse<KbFoodCategoryGroup>> {
+
+  // }
 }

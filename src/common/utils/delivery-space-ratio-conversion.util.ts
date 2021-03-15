31a0fb63aaed173dc0b4df16097f0d2757241265
bbogeul <illumeweb@gmail.com>
2021-03-15 11:13:31 +0900

    updated ratio class to object

--- src/common/utils/delivery-space-ratio-conversion.util.ts
+++ src/common/utils/delivery-space-ratio-conversion.util.ts
@@ -1,11 +1,11 @@
 export const DeliverySpaceConversion = (deliveryRatio: number) => {
-  let grade: number;
+  let grade;
   if (deliveryRatio > 40) {
-    grade = 1;
+    grade = { grade: 1, key: 'deliveryData' };
   } else if (deliveryRatio > 30 && deliveryRatio < 40) {
-    grade = 2;
+    grade = { grade: 2, key: 'restaurantData' };
   } else {
-    grade = 3;
+    grade = { grade: 3, key: 'aggregateData' };
   }
 
   return grade;

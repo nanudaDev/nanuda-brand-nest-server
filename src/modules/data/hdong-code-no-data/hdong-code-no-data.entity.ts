0efdb8b2183657c4c919d3d4a80cd87d18a2c8d8
bbogeul <illumeweb@gmail.com>
2021-03-21 15:05:31 +0900

    hdong code no data table

---
+++ src/modules/data/hdong-code-no-data/hdong-code-no-data.entity.ts
@@ -0,0 +1,24 @@
+import { YN } from 'src/common';
+import { BaseEntity } from 'src/core';
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'hdong_code_no_data' })
+export class HdongCodeNoData extends BaseEntity<HdongCodeNoData> {
+  @Column({
+    name: 'hdong_code',
+    type: 'varchar',
+  })
+  hdongCode: string;
+
+  @Column({
+    type: 'varchar',
+  })
+  endpoint: string;
+
+  @Column({
+    name: 'is_usable',
+    type: 'char',
+    default: () => YN.NO,
+  })
+  isUsable: YN;
+}

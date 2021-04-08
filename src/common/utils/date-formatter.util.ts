5966f260100a37398acc20383b85417dd8350cb8
eddie-nanuda <lsh@widaehan.com>
2021-04-08 14:40:50 +0900

    console.log date formatter

--- src/common/utils/date-formatter.util.ts
+++ src/common/utils/date-formatter.util.ts
@@ -1,4 +1,10 @@
 export const DateFormatter = (date: Date | string) => {
+  console.log(
+    new Date(date)
+      .toLocaleString('ko-KR')
+      .substr(0, 10)
+      .split(' ')[0],
+  );
   return new Date(date)
     .toLocaleString('ko-KR')
     .substr(0, 10)
